21b9f67390de7455026cf2b1af7dea0084837130
abnerolimor <abner.oliveira.moraes@escola.pr.gov.br>
2025-04-28 11:58:08 -0300

    Update projeto_banco.sql

--- projeto_banco.sql
+++ projeto_banco.sql
@@ -1,10 +1,10 @@
-create database responsavel;
+create database pet;
 
-use responsavel;
+use pet;
 
 
 
-CREATE TABLE IF NOT EXISTS `responsavel`.`responsavel_pet` (
+CREATE TABLE IF NOT EXISTS `pet`.`responsavel_pet` (
   `idresponsavel` INT NOT NULL,
   `nome` VARCHAR(45) NOT NULL,
   `email` VARCHAR(45) NOT NULL,
@@ -13,4 +13,22 @@ CREATE TABLE IF NOT EXISTS `responsavel`.`responsavel_pet` (
   `endereco` VARCHAR(45) NOT NULL,
   `senha` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`idresponsavel`))
+ENGINE = InnoDB
+
+CREATE TABLE IF NOT EXISTS `pet`.`Endereco` (
+  `idEndereco` INT NOT NULL,
+  `Rua` VARCHAR(255) NOT NULL,
+  `bairro` VARCHAR(255) NOT NULL,
+  `cep` VARCHAR(255) NOT NULL,
+  `complemento` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`idEndereco`))
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `pet`.`Pet_shop` (
+  `idPet_shop` INT NOT NULL,
+  `nome` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `cnpj` VARCHAR(255) NOT NULL,
+  `telefone` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`idPet_shop`))
 ENGINE = InnoDB
